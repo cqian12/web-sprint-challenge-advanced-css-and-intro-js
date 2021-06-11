dcf5832f60f75073a6a37de5999e8774efc269cb
Charles Qian <charleshqian@gmail.com>
2021-06-11 12:01:42 -0400

    completed responsive styling, started JS stretch tasks

--- index.js
+++ index.js
@@ -326,7 +326,7 @@ function lotsOfArt(array){
   return newArray
 }
 
-console.log(lotsOfArt(artists))
+//console.log(lotsOfArt(artists))
 
 
 // 🎨🎨 STRETCH 🎨🎨//
@@ -351,7 +351,7 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(data){
+function getHTML(/*tbd*/){
 
     /* Code here */
 
@@ -361,12 +361,18 @@ function getHTML(data){
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
-
-    /* Code here */
-
+function randomize(array){
+  let newArray = []
+  
+  for (let i = 0; i < array.length; i++) {
+    let randomNum = Math.random() * array.length
+    newArray.push(array[randomNum])
+    array.splice(randomNum,1)
   }
+  return newArray
+}
 
+console.log(randomize(artists))
 
  /* 💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪
  Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
